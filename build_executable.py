7c4a410c3cdcddaa1e9f9cc2ca25ba52541d4ff1
Dahicup1032 <tonynow0@gmail.com>
2026-02-18 19:37:19 -0600

    Add build script for Windows executable

---
+++ build_executable.py
@@ -0,0 +1,11 @@
+# build_executable.py
+
+import os
+import sys
+
+def build_executable():
+    # Placeholder for the build logic
+    print("Building Windows executable...")
+
+if __name__ == '__main__':
+    build_executable()
